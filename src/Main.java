9ef18d5caf0953cbd304027edbcad370d54daf1a
Dhiraj  Nimbalkar <158476919+DhirajNimbalkar001@users.noreply.github.com>
2024-11-16 20:33:10 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
         System.out.println("Hello, Docker! Current date: " + currentDate);
+        
     }
+    
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    // In order to use PUT HTTP verb to edit item
+    methodOverride = require('method-override'),
+    // Mitigate XSS using sanitizer
+    sanitizer = require('sanitizer'),
+    app = express(),
+    port = 8000
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 }
